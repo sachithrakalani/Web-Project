2b65ea7ef206b20aa807bef1c32b81b8a36122ed
sachithrakalani <kalanisathyangi@gmail.com>
2022-12-27 12:49:09 +0530

    connect to login page

--- menu.php
+++ menu.php
@@ -1,3 +1,59 @@
+<?php
+// connect to database
+//$conn = mysqli_connect('localhost','root','','pizza_store');
+
+//.check .connection
+//if(!$conn){
+// echo 'Connection.error:' . mysqli_connect_error();
+//}
+
+// write query for all pizza categori
+//$sql = 'SELECT * FROM Our_Menu';
+
+//make query & get result
+//$result = mysqli_query($conn, $sql);
+//  echo  $result;
+
+//  // fetch the resulting rows as an arry
+//$pizza_store = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//free result from memory
+//mysqli_free_result($result);
+
+// close connection
+//mysqli_close($conn);
+//echo ($pizza_store);
+//  echo '<script>console.log("'$var1 . "\n" . $var2'"); </script>';
+
+
+
+
+
+
+
+
+function goClick()
+{
+    echo 'abd';
+}
+
+
+
+
+if (isset($_POST['btn05'])) {
+    echo "This is Button1 that is selected";
+}
+
+?>
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -35,8 +91,6 @@
             <div class="header" id="head02">
 
                 <h1>Our Menu</h1>
-
-
                 <ul class="menu" id="menu02">
 
                     <li class="button active" id="ital">
@@ -70,7 +124,7 @@
                         <!-- <h3>pizza 1</h3> -->
                         <h4>BBQ Chicken Pizza0</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';" >Go</a>
                 </div>
 
 
@@ -82,7 +136,7 @@
                         <!-- <h3>pizza 2</h3> -->
                         <h4>Acapulco Chicken Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
 
@@ -95,7 +149,7 @@
                         <!-- <h3>pizza 3</h3> -->
                         <h4>Buffolo Chicken Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
 
@@ -108,7 +162,7 @@
                         <!-- <h3>pizza 4</h3> -->
                         <h4>Chicken Bell pepper Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
 
@@ -121,7 +175,7 @@
                         <!-- <h3>pizza 5</h3> -->
                         <h4>Spicy Chicken Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
 
@@ -134,7 +188,7 @@
                         <!-- <h3>pizza 6</h3> -->
                         <h4>Thai Chicken Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
 
@@ -179,7 +233,7 @@
 
                 <!-- Cheese -->
 
-                
+
                 <div class="card veg" id="cardmenu01">
                     <div class="img" id="crdimg">
                         <img src="images/5.jpg" alt="">
@@ -188,7 +242,7 @@
                         <!-- <h3>pizza 11</h3> -->
                         <h4>Cheese Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
 
@@ -200,7 +254,7 @@
                         <!-- <h3>pizza 12</h3> -->
                         <h4>Cheeseburger Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
 
@@ -212,7 +266,7 @@
                         <!-- <h3>pizza 13</h3> -->
                         <h4>Cheese and papperoni Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
 
@@ -224,7 +278,7 @@
                         <!-- <h3>pizza 14</h3> -->
                         <h4>Cheese Bread Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
 
@@ -236,7 +290,7 @@
                         <!-- <h3>pizza 15</h3> -->
                         <h4>Cheesy Pepper Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
                 <div class="card veg" id="cardmenu01">
@@ -247,7 +301,7 @@
                         <!-- <h3>pizza 16</h3> -->
                         <h4>Cherry Cheese Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
                 <!-- <div class="card veg" id="cardmenu01">
@@ -256,7 +310,7 @@
                     </div>
                     <div class="text-desk" id="txdesk">
                         <!-- <h3>pizza 10</h3> -->
-                        <!-- <h4>vegetarian</h4>
+                <!-- <h4>vegetarian</h4>
                     </div>
                     <a href="#" class="btn" id="btn05">Go</a>
                 </div> --> -->
@@ -272,7 +326,7 @@
                         <!-- <h3>pizza 21</h3> -->
                         <h4>Apple Pizza Dessert</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
                 <div class="card ital" id="cardmenu01">
@@ -283,7 +337,7 @@
                         <!-- <h3>pizza 22</h3> -->
                         <h4>Apple Pie Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
                 <div class="card ital" id="cardmenu01">
@@ -294,7 +348,7 @@
                         <!-- <h3>pizza 23</h3> -->
                         <h4>Seafood Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
                 <div class="card ital" id="cardmenu01">
@@ -305,7 +359,7 @@
                         <!-- <h3>pizza 24</h3> -->
                         <h4>Mixed Berry Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
                 <div class="card ital" id="cardmenu01">
@@ -316,7 +370,7 @@
                         <!-- <h3>pizza 25</h3> -->
                         <h4>Cannoli Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
 
@@ -328,7 +382,7 @@
                         <!-- <h3>pizza 26</h3> -->
                         <h4>Thriple Berry Pizza</h4>
                     </div>
-                    <a href="#" class="btn" id="btn05">Go</a>
+                    <a href="#" class="btn" id="btn05" onclick="window.location.href='login.php';">Go</a>
                 </div>
 
             </div>
@@ -346,12 +400,6 @@
 
 
     </div>
-
-
-
-
-
-
 
     <script src="jquery-3.6.2.min.js"></script>
     <script src="main.js"></script>
